90ff8433e678fa58ceec8ace96a3aac7623f6383
Eduardo <eduardo.dev69@gmail.com>
2025-11-04 13:11:42 -0300

    iniciando o projeto

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+   //Ver se estamos na página de login ou na de produtos
+    const paginaLogin = document.getElementById("formLogin");
+    const paginaProdutos = document.getElementById("formProduto");)
