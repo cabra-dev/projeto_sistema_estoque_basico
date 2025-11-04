0ea057d9591504a8e3aaed351a1bf2fbf88149ee
Eduardo <eduardo.dev69@gmail.com>
2025-11-04 13:18:59 -0300

    captura os valores digitados pelo usuário

--- js/app.js
+++ js/app.js
@@ -2,3 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
    //Ver se estamos na página de login ou na de produtos
     const paginaLogin = document.getElementById("formLogin");
     const paginaProdutos = document.getElementById("formProduto");)
+
+    // LOGIN SIMPLES
+    if (paginaLogin) {
+        paginaLogin.addEventListener("submit", (event) => {
+            event.preventDefault(); // impede o recarregamento da página
+
+            // Captura os valores dos campos de entrada
+            const user = document.getElementById("user").value.trim();
+            const pass = document.getElementById("pass").value.trim();
