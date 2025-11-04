d491b5a2e3c3819c9d41af9388966d973d500c2e
Eduardo <eduardo.dev69@gmail.com>
2025-11-04 13:22:42 -0300

    verifica se o usário é valido

--- js/app.js
+++ js/app.js
@@ -11,3 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
             // Captura os valores dos campos de entrada
             const user = document.getElementById("user").value.trim();
             const pass = document.getElementById("pass").value.trim();
+
+            // Verifica se os campos não estão vazios
+            if (user === "admin" && pass === "1234") {
+                // Redireciona para a página de produtos
+                window.location.href = "produtos.php";
+            } else {
+                // Exibe uma mensagem de erro
+                alert("Usuário ou senha incorretos!");
+            }
