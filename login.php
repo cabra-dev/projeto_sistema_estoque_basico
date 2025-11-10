07744161a1d4cd12d8ab0fe58a3db402e6d4b38a
Eduardo <eduardo.dev69@gmail.com>
2025-11-10 10:24:56 -0300

    apagando um paragrafo da tela de login

--- login.php
+++ login.php
@@ -21,7 +21,6 @@
         <input type="password" id="pass" required>
         <button type="submit" class="btn">Entrar</button>
       </form>
-      <p class="hint">Usuário: <b>admin</b> | Senha: <b>1234</b></p>
     </div>
   </div>
   <script src="js/app.js"></script>
